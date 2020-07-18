d6406cc6b858baf76913c3f5ce1226d70ad2d9d8
Dan Swan <rodger.swan@gmail.com>
2020-07-18 18:09:49 +1000

    full set of questions, readme nearly formatted

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
-# Project Title:
-README generator
-## Project Description:
-this generates a markdown readme file through node.js and inquirer module
----
-## Table of Contents
+# Project Title: kjsdalfj
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+Developed by codenswan
 
-## Installation:
-    npm install    
+### Table of Contents
+*[Description](# Project Description:)
+*[Install Instructions](# Installation:)
+*[Usage]()
+*[License]()
+*[Contributing]()
+*[Tests]()
+*[Questions](### Questions:)
+
+### Project Description:
+daslfkj
+
+### Installation:
+    sdflkjasdf
+    
+### Usage:
+
 ---
-## Questions:
-Please contact me via email [mailto:rodger.swan@gmail.com] if you have any questions about this project.
+### Questions:
+Please contact me via email [rodger.swan@gmail.com](mailto:rodger.swan@gmail.copm) if you have any questions about this project.
--- index.js
+++ index.js
@@ -1,54 +1,107 @@
-// const inquirer = require("inquirer");
-// const fs = require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-// inquirer
-//   .prompt([
-//     {
-//       type: "input",
-//       name: "title",
-//       message: "Enter the title of your project:",
-//     },
-//     {
-//       type: "input",
-//       name: "description",
-//       message: "Describe your project:",
-//     },
-//     {
-//       type: "input",
-//       name: "installation",
-//       message: "What are the installation instructions?"
-//     },
-//     {
-//       type: "input",
-//       name: "GitHub",
-//       message: "What is your GitHub username?:",
-//     },
-//     {
-//       type: "input",
-//       name: "email",
-//       message: "What is your email address?:",
-//     },
-//   ])
-//   .then(function (data) {
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter the title of your project:",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Describe your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the installation instructions?"
+    },
+    {
+     type: "input",
+     name: "usage",
+     message: "Enter information about the usage", 
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "What is the contribution information?"
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "What are the test instructions?"
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What license are you using?",
+      choices: 
+      [
+        "MIT", 
+        "Mozilla Public 2.0", 
+        "Apache 2.0", 
+        "Boost Software 1.0",
+        "GNU GPLv3",
+      ]
+    },
+    {
+      type: "input",
+      name: "GitHub",
+      message: "What is your GitHub username?:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?:",
+    },  
+  ]);
+};
+
+function generateREADME(answers) {
+  return `# Project Title: ${answers.title}
+![${answers.license}](https://img.shields.io/github/license/codenswan/09-Node.js-README-Generator?style=flat)
+
+### Table of Contents
+*[Description](# Project Description:)
+*[Install Instructions](# Installation:)
+*[Usage]()
+*[License]()
+*[Contributing]()
+*[Tests]()
+*[Questions](### Questions:)
+
+### Project Description:
+${answers.description}
+
+### Installation:
+    ${answers.installation}
     
-//     fs.writeFile(
-//       "README.md",
-//       `# Project Title: ${data.title}
-// ## Description
-// ${data.description}
-// ---
-// ## Questions
-// ### GitHub profile: ${data.GitHub}
-// ### Email: [mailto:${data.email}]
-// `,
-//       function (err) {
-//         if (err) {
-//           return console.log(err);
-//         }
+### Usage:
 
-//         console.log("Success!");
-//       }
-//     );
-//   });
+---
+### Questions:
+Developed by ${answers.GitHub}. Please contact me via email [${answers.email}](mailto:${answers.email}) if you have any questions about this project.
+`;
+}
 
+async function init() {
+  console.log("hi")
+  try {
+    const answers = await promptUser();
 
+    const readme = generateREADME(answers);
+
+    await writeFileAsync("README.md", readme);
+
+    console.log("Successfully wrote your README file");
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+init();
